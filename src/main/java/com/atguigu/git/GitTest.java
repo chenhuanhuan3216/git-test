1cb497b674e45bc33e2ca76326434c7f0bde433f
Jerry <Jerry@atguigu.com>
2021-11-11 16:13:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ChenJian
+ * @create 2021-11-11 16:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
