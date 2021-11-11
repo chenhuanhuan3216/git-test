bb2414f04313b6194b0f386930a286965c2ef586
Jerry <Jerry@atguigu.com>
2021-11-11 16:25:27 +0800

    hot-fix commit_1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git!!");
         System.out.println("Hello Git222!!");
         System.out.println("Hello Git333!!");
+        System.out.println("Hello Git444!!");
     }
 }
